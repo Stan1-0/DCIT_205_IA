35980d6b0ddaa799751e59b534b523145580b606
Stan1-0 <stankofb@gmail.com>
2023-11-22 19:00:48 +0000

    changed the footer

--- 11253377_DCIT105/app.js
+++ 11253377_DCIT105/app.js
@@ -1,17 +1,15 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
-)
-
-let sun = document.getElementById("sun");
-sun.onclick = function(){
-    sun.classList.toggle("night")
-}
-function myFunction(){
-    let element = document.body;
-    element.classList.toggle("dark-mode");
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
 }
